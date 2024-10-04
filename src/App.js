9e488d967e7e011f66609d697bcdb8c6164cb961
Nora Keavney <g00415845@atu.ie>
2024-10-04 11:02:31 +0000

    Exercise 5 & 6 : editing app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/nav';
 import Header from './components/Header';
-import Content from './components/content';
 import Footer from './components/Footer';
+import Content from './components/content';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Content />
-      <Footer />
-    </div>
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<Footer /> } />
+        <Route path="/create" element={<Header />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
