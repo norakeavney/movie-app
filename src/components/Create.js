bed5dfbdfba94c0b660fde9a499853ca938cf2c9
Nora Keavney <g00415845@atu.ie>
2024-10-18 10:58:01 +0000

    Basic Create form

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,13 +1,32 @@
-const Create = () => {
-   
-    return(
-        <div>
-            <h3>  Create Contents  </h3>
+import { useState } from "react";
+
+function Create() {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
+  }
+
+  return (
+    <div>
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>
+
+
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
         </div>
-    );
         
-    
-  };
-  
-  //export to app
-  export default Create;
+        <input type="submit" value="Add Movie" />
+      </form>
+    </div>
+  );
+}
+
+export default Create;
--- src/components/Read.js
+++ src/components/Read.js
@@ -3,17 +3,21 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import Movies from './Movies'
 const Read = ()=>{
+    //Movies: variable, setMovies: function to update movies 
    const [movies, setMovies] = useState([])
-
+        
+        //Continuously Running
         useEffect(
             () => {
                 //asynchronisation
-                //Retrieve 
+                //Retrieve data from API using axios
                 axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
+                //Once retrieved response then display using setMovies - function to update the variable movies 
                 .then( (response) => {
                     console.log(response.data)
                     setMovies(response.data.movies)
                 })
+                //Error Handling
                 .catch((error) => {
                     console.log(error);
                 }
